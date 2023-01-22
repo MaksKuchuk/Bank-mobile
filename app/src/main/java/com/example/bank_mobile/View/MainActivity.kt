84f6a415f4550db5ce7af0fbc578efe8ca134f7e
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2023-01-22 21:29:45 +1000

    mainScreen

--- app/src/main/java/com/example/bank_mobile/View/MainActivity.kt
+++ app/src/main/java/com/example/bank_mobile/View/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.bank_mobile.View
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bank_mobile.R
+import com.example.bank_mobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnListOfPlaces.setOnClickListener {
+                Intent(this@MainActivity, ListOfPlacesActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnLoanOnline.setOnClickListener {
+                Intent(this@MainActivity, LoanOnlineActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnVerification.setOnClickListener {
+                Intent(this@MainActivity, VerificationActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnMyProfile.setOnClickListener {
+                Intent(this@MainActivity, ProfileActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnShowMap.setOnClickListener {
+                Intent(this@MainActivity, ShowMapActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 }
